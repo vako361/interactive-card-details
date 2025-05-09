c84b7302f547a3ad6c58e163f778e118776ad580
vako <vakokoblianidze12334@gmail.com>
2025-05-09 19:01:38 +0400

    finished mobile

--- src/components/Verify.tsx
+++ src/components/Verify.tsx
@@ -7,7 +7,7 @@ const Veryfy = () => {
       <Input placeholder='e.g. Jane Appleseed'></Input>
       </div>
         <div className='w-[327px] flex flex-col items-start justify-center gap-[9px] font-[500]'>
-     <p className='self-start text-[var(--Deep-Violet, #21092F)] text-[12px]'>Cardholder Name</p>
+     <p className='self-start text-[var(--Deep-Violet, #21092F)] text-[12px]'>Card Number</p>
       <Input placeholder='e.g. 1234 5678 9123 0000'></Input>
       </div>
           <div className='w-[327px] flex flex-col items-start justify-center gap-[9px] font-[500]'>
@@ -17,11 +17,15 @@ const Veryfy = () => {
         <p className='self-start text-[var(--Deep-Violet, #21092F)] text-[12px]'>CVC</p> 
      </div>
 
-<div>
-      <Input placeholder='e.g. 1234 5678 9123 0000'></Input>
-</div>
+<div className='flex flex-row items-start justify-between gap-[9px]'>
+      <input className='w-[72px] h-[45px] ' id='in' placeholder='MM'/>
+      <input className='w-[72px] h-[45px] ' id='inp' placeholder='YY'/>
+      <input className='w-[72px] h-[45px] ' id='inpu' placeholder='e.g. 123'/>
+</div> 
 
-      </div>
+ </div>
+
+    <button className='w-[327px] h-[53px] bg-[#21092F] rounded-[8px]  text-white'>Confirm</button>
 
     </div>
   );
